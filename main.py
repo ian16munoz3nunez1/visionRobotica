8ab3283e865c9ebc6344d11dc9a8768ebc65e5b8
ian16munoz3nunez1 <ianemn1603@gmail.com>
2023-01-01 14:09:18 -0600

    Punto B

--- main.py
+++ main.py
@@ -2,13 +2,11 @@ import cv2 # Se importa la opencv --> pip install opencv-contrib-python
 import numpy as np # Se importa la libreria numpy como np --> pip install numpy
 import matplotlib.pyplot as plt # Se importa la libreria pyplot --> pip install matplotlib
 
-imagen = np.zeros((8, 15), dtype=np.uint8) # Se crea una imagen de 8x15
+imagen = np.zeros((12, 6), dtype=np.uint8) # Se crea una imagen de 12x6
 
-imagen[2, 3] = 255 # Se asigna un valor de 255 al pixel en la posicion (2, 3)
+imagen[1:3, 1:5] = 200 # Se asigna un valor de 200 desde la posicion (1, 1) hasta la (2, 4)
 
-imagen[2, 11] = 170 # Se asigna un valor de 170 al pixel en la posicion (2, 11)
-
-imagen[5, 7] = 85 # Se asigna el balor de 85 al pixel en la posicion (5, 7)
+imagen[4:11, 1:5] = 150 # Se asigna un valor de 150 desde la posicion (4, 1) hasta la (10, 4)
 
 plt.imshow(imagen, cmap="gray") # Se agrega la imagen al pyplot
 
