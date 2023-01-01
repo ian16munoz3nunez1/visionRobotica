805d8739adb2da461fa0c16462ee83de2b4abf83
ian16munoz3nunez1 <ianemn1603@gmail.com>
2023-01-01 15:11:10 -0600

    Punto D

--- main.py
+++ main.py
@@ -2,19 +2,19 @@ import cv2 # Se importa la opencv --> pip install opencv-contrib-python
 import numpy as np # Se importa la libreria numpy como np --> pip install numpy
 import matplotlib.pyplot as plt # Se importa la libreria pyplot --> pip install matplotlib
 
-imagen = np.zeros((9, 8, 3), dtype=np.uint8) # Se crea una imagen de 9x8x3
+imagen = np.zeros((16, 8, 3), dtype=np.uint8) # Se crea una imagen de 16x8x3
 
-# Se asigna el color cian al pixel en la posicion (1, 1)
-imagen[1, 1, 0] = 255
-imagen[1, 1, 1] = 255
+# Se asigna el color (125, 225, 0) desde la posicion (1, 1) a la (2, 6)
+imagen[1:3, 1:7, 0] = 125
+imagen[1:3, 1:7, 1] = 225
 
-# Se asigna el color magenta al pixel en la posicion (3, 6)
-imagen[3, 6, 0] = 255
-imagen[3, 6, 2] = 255
+# Se asigna el color (100, 0, 250) desde la posicion (4, 2) a la (10, 5)
+imagen[4:11, 2:6, 0] = 100
+imagen[4:11, 2:6, 2] = 250
 
-# Se asigna el color amarillo al pixel en la posicion (7, 3)
-imagen[7, 3, 1] = 255
-imagen[7, 3, 2] = 255
+# Se asigna el color (0, 200, 190) desde la posicion (12, 1) a la (14, 6)
+imagen[12:15, 1:7, 1] = 200
+imagen[12:15, 1:7, 2] = 190
 
 # Se cambia el formato de la imagen de BGR a RGB para mostrarlo en el pyplot
 rgb = cv2.cvtColor(imagen, cv2.COLOR_BGR2RGB)
