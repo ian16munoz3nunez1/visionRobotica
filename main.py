89cb6e8998e79c50eedf389b3e3c3386f0c9ee68
ian16munoz3nunez1 <ianemn1603@gmail.com>
2023-01-01 17:23:34 -0600

    Metodo por promedio

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import cv2 # Se importa opencv
 import numpy as np # Se importa numpy como np
-from numpy.lib.function_base import copy # Se importa el modulo copy
 
 imagen = cv2.imread("rgb.jpg") # Se lee la imagen
 
@@ -8,11 +7,13 @@ R = imagen[:, :, 2] # Se crea una escala de grises desde el canal R
 G = imagen[:, :, 1] # Se crea una escala de grises desde el canal G
 B = imagen[:, :, 0] # Se crea una escala de grises desde el canal B
 
-RGB = np.vstack((R, G, B)) # Se concatenan las escalas creadas
+# Se crea un filtro para una escala de grises con el promedio de los canales
+promedio = R*0.33 + G*0.33 + B*0.33
+promedio = promedio.astype(np.uint8) # Se convierte el promedio a uint8
 
 # Se muestran las imagenes
 cv2.imshow("Imagen", imagen)
-cv2.imshow("RGB", RGB)
+cv2.imshow("Promedio", promedio)
 
 # Se espera a que el usuario presione la tecla 'esc'
 while True:
