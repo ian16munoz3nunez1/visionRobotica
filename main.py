32b13d9b1b816087082e163fe3ac4ad203f173e1
ian16munoz3nunez1 <ianemn1603@gmail.com>
2023-01-01 05:08:11 -0600

    Librerias necesarias

---
+++ main.py
@@ -0,0 +1,4 @@
+import cv2 # Se importa la opencv --> pip install opencv-contrib-python
+import numpy as np # Se importa la libreria numpy como np --> pip install numpy
+import matplotlib.pyplot as plt # Se importa la libreria pyplot --> pip install matplotlib
+
