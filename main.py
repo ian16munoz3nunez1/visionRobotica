9d46b5c239ae6f6b0827d32dbdb90cd666f51943
ian16munoz3nunez1 <ianemn1603@gmail.com>
2023-01-01 17:09:24 -0600

    Metodo RGB

--- main.py
+++ main.py
@@ -4,28 +4,15 @@ from numpy.lib.function_base import copy # Se importa el modulo copy
 
 imagen = cv2.imread("rgb.jpg") # Se lee la imagen
 
-# Se crea una copia de la imagen para aplicar los filtros
-r = copy(imagen)
-g = copy(imagen)
-b = copy(imagen)
+R = imagen[:, :, 2] # Se crea una escala de grises desde el canal R
+G = imagen[:, :, 1] # Se crea una escala de grises desde el canal G
+B = imagen[:, :, 0] # Se crea una escala de grises desde el canal B
 
-# Se asigna un valor de 0 a los canales B y G
-r[:, :, 0] = 0
-r[:, :, 1] = 0
-
-# Se asigna un valor de 0 a los canales B y R
-g[:, :, 0] = 0
-g[:, :, 2] = 0
-
-# Se asigna un valor de 0 a los canales G y R
-b[:, :, 1] = 0
-b[:, :, 2] = 0
-
-filtros = np.vstack((r, g, b)) # Se concatenan los filtros aplicados
+RGB = np.vstack((R, G, B)) # Se concatenan las escalas creadas
 
 # Se muestran las imagenes
 cv2.imshow("Imagen", imagen)
-cv2.imshow("Filtros", filtros)
+cv2.imshow("RGB", RGB)
 
 # Se espera a que el usuario presione la tecla 'esc'
 while True:
