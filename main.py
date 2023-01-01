40a7649e6dd5f0da93818c5c38590192100d412a
ian16munoz3nunez1 <ianemn1603@gmail.com>
2023-01-01 17:43:07 -0600

    Promedio, BT.601 y BT.709

--- main.py
+++ main.py
@@ -7,13 +7,21 @@ R = imagen[:, :, 2] # Se crea una escala de grises desde el canal R
 G = imagen[:, :, 1] # Se crea una escala de grises desde el canal G
 B = imagen[:, :, 0] # Se crea una escala de grises desde el canal B
 
-# Se crea un filtro para una escala de grises con el metodo BT.709
+# Metodo por promedio
+promedio = R*0.33 + G*0.33 + B*0.33
+promedio = promedio.astype(np.uint8)
+# Metodo BT.601
+bt_601 = R*0.299 + G*0.587 + B*0.114
+bt_601 = bt_601.astype(np.uint8)
+# Metodo BT.709
 bt_709 = R*0.2126 + G*0.7152 + B*0.0722
-bt_709 = bt_709.astype(np.uint8) # Se convierte el BT.709 a uint8
+bt_709 = bt_709.astype(np.uint8)
+
+grises = np.vstack((promedio, bt_601, bt_709)) # Se concatenan los filtros
 
 # Se muestran las imagenes
 cv2.imshow("Imagen", imagen)
-cv2.imshow("BT.709", bt_709)
+cv2.imshow("Grises", grises)
 
 # Se espera a que el usuario presione la tecla 'esc'
 while True:
