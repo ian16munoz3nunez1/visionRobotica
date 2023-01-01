8a0b45971b5b3dced177c5ce158007d744e7ac36
ian16munoz3nunez1 <ianemn1603@gmail.com>
2023-01-01 17:32:16 -0600

    Metodo BT.601

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ R = imagen[:, :, 2] # Se crea una escala de grises desde el canal R
 G = imagen[:, :, 1] # Se crea una escala de grises desde el canal G
 B = imagen[:, :, 0] # Se crea una escala de grises desde el canal B
 
-# Se crea un filtro para una escala de grises con el promedio de los canales
-promedio = R*0.33 + G*0.33 + B*0.33
-promedio = promedio.astype(np.uint8) # Se convierte el promedio a uint8
+# Se crea un filtro para una escala de grises con el metodo BT.601
+bt_601 = R*0.299 + G*0.587 + B*0.114
+bt_601 = bt_601.astype(np.uint8) # Se convierte el BT.601 a uint8
 
 # Se muestran las imagenes
 cv2.imshow("Imagen", imagen)
-cv2.imshow("Promedio", promedio)
+cv2.imshow("BT.601", bt_601)
 
 # Se espera a que el usuario presione la tecla 'esc'
 while True:
