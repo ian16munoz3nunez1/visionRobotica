692aa1e34556a88e5ddc6d978b5b56effcf2d87e
ian16munoz3nunez1 <ianemn1603@gmail.com>
2023-01-01 19:44:46 -0600

    Filtros aplicados a la imagen del punto C de la actividad 1

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import cv2 # Se importa opencv
 import numpy as np # Se importa numpy como np
+import matplotlib.pyplot as plt # Se importa matplotlib como plt
 
-imagen = cv2.imread("rgb.jpg") # Se lee la imagen
+imagen = np.zeros((9, 8, 3), dtype=np.uint8) # Se crea una imagen de 9x8x3
+
+# Se asigna el color cian al pixel en la posicion (1, 1)
+imagen[1, 1, 0] = 255
+imagen[1, 1, 1] = 255
+
+# Se asigna el color magenta al pixel en la posicion (3, 6)
+imagen[3, 6, 0] = 255
+imagen[3, 6, 2] = 255
+
+# Se asigna el color amarillo al pixel en la posicion (7, 3)
+imagen[7, 3, 1] = 255
+imagen[7, 3, 2] = 255
 
 R = imagen[:, :, 2] # Se crea una escala de grises desde el canal R
 G = imagen[:, :, 1] # Se crea una escala de grises desde el canal G
@@ -17,17 +30,24 @@ bt_601 = bt_601.astype(np.uint8)
 bt_709 = R*0.2126 + G*0.7152 + B*0.0722
 bt_709 = bt_709.astype(np.uint8)
 
-RGB = np.vstack((R, G, B)) # Se conctatenan las matrices RGB
+RGB = np.vstack((R, G, B)) # Se concatenan las matrices RGB
 grises = np.vstack((promedio, bt_601, bt_709)) # Se concatenan los ponderaciones
+imagen = cv2.cvtColor(imagen, cv2.COLOR_BGR2RGB) # Se cambia el formato de BGR a RGB
 
-# Se muestran las imagenes
-cv2.imshow("Imagen", imagen)
-cv2.imshow("RGB", RGB)
-cv2.imshow("Grises", grises)
+# Se muestra la imagen original
+plt.figure(1)
+plt.imshow(imagen)
+plt.title("Imagen")
 
-# Se espera a que el usuario presione la tecla 'esc'
-while True:
-    if cv2.waitKey(1) == 27:
-        break
-cv2.destroyAllWindows() # Se cierran las ventanas
+# Se muestran los filtros RGB de la imagen
+plt.figure(2)
+plt.imshow(RGB, cmap="gray")
+plt.title("RGB")
+
+# Se muestran las ponderaciones de la imagen
+plt.figure(3)
+plt.imshow(grises, cmap="gray")
+plt.title("Grises")
+
+plt.show()
 
