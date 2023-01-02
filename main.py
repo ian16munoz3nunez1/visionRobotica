2333b4d5c1d45bd1756a8d1d67eac102ee7d89ee
ian16munoz3nunez1 <ianemn1603@gmail.com>
2023-01-01 20:18:59 -0600

    Ejercicio 1

---
+++ main.py
@@ -0,0 +1,20 @@
+import cv2 # Se importa opencv
+import numpy as np # Se importa numpy como np
+
+imagen = cv2.imread("rgb.jpg") # Se lee la imagen
+
+grises = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY) # Se convierte la imagen a escala de grises
+
+negativa = 255 - grises # Se obtiene la negativa de la imagen en escala de grises
+
+# Se muestran las imagenes
+cv2.imshow("Imagen", imagen)
+cv2.imshow("Grises", grises)
+cv2.imshow("Negativa", negativa)
+
+# Se espera a que el usuario pulse la tecla 'esc'
+while True:
+    if cv2.waitKey(1) == 27:
+        break
+cv2.destroyAllWindows() # Se cierran todas las ventanas
+
