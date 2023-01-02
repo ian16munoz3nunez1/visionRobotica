cb281caec4dd703fc6afb20d26907336e35ee99f
ian16munoz3nunez1 <ianemn1603@gmail.com>
2023-01-01 20:25:51 -0600

    Ejercicio 3

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ import numpy as np # Se importa numpy como np
 
 imagen = cv2.imread("rgb.jpg") # Se lee la imagen
 
-negativa = 255 - imagen # Se obtiene la negativa de la imagen
+grises = cv2.rotate(imagen, cv2.ROTATE_90_CLOCKWISE) # Se rota la imagen original 90 grados
+grises = cv2.cvtColor(grises, cv2.COLOR_BGR2GRAY) # Se convierte la imagen a escala de grises
+
+negativa = 255 - grises # Se obtiene la negativa de la imagen en escala de grises
 
 # Se muestran las imagenes
 cv2.imshow("Imagen", imagen)
+cv2.imshow("Grises", grises)
 cv2.imshow("Negativa", negativa)
 
 # Se espera a que el usuario pulse la tecla 'esc'
