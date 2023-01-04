cc8f825b0e6f4c7b690ea4f34f8e851d5ea98653
ian16munoz3nunez1 <ianemn1603@gmail.com>
2023-01-04 02:44:11 -0600

    Ejercicios SATURATION

--- main.py
+++ main.py
@@ -1,37 +1,32 @@
-# Generar imagenes de distinto color usando el modelo HSV
-# 1. Generar una imagen de color amarillo
-# 2. Generar una imagen de color naranja
-# 3. Generar una imagen de color cian
-# 4. Generar una imagen de color magenta
-# 5. Generar una imagen de color azul
+# Usar distintos niveles de saturación para observar la diferencia
 
 import cv2 # Se importa la opencv
 import numpy as np # Se importa la libreria numpy como np
 
-s = np.zeros((480, 640), dtype=np.uint8) + 255 # Se asigna el nivel al campo 'saturation'
+h = np.zeros((480, 640), dtype=np.uint8) + 30 # Se asigna el nivel al campo de 'hue' para el color amarillo
 v = np.zeros((480, 640), dtype=np.uint8) + 255 # Se asigna el nivel al campo 'value'
 
-h = np.zeros((480, 640), dtype=np.uint8) + 30 # Se asigna el nivel al campo de 'hue' para el color amarillo
+s = np.zeros((480, 640), dtype=np.uint8) + 0 # Se asigna el nivel al campo 'saturation'
 hsv = cv2.merge((h, s, v)) # Se mezclan los canales 'hue', 'saturation' y 'value'
 imagen = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR) # Se convierte el espacio de color de 'HSV' a 'BGR'
 cv2.imshow("Imagen 1", imagen) # Se muestra la imagen 1
 
-h = np.zeros((480, 640), dtype=np.uint8) + 15 # Se asigna el nivel al campo de 'hue' para el color naranja
+s = np.zeros((480, 640), dtype=np.uint8) + 50 # Se asigna el nivel al campo 'saturation'
 hsv = cv2.merge((h, s, v)) # Se mezclan los canales 'hue', 'saturation' y 'value'
 imagen = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR) # Se convierte el espacio de color de 'HSV' a 'BGR'
 cv2.imshow("Imagen 2", imagen) # Se muestra la imagen 1
 
-h = np.zeros((480, 640), dtype=np.uint8) + 90 # Se asigna el nivel al campo de 'hue' para el color cian
+s = np.zeros((480, 640), dtype=np.uint8) + 100 # Se asigna el nivel al campo 'saturation'
 hsv = cv2.merge((h, s, v)) # Se mezclan los canales 'hue', 'saturation' y 'value'
 imagen = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR) # Se convierte el espacio de color de 'HSV' a 'BGR'
 cv2.imshow("Imagen 3", imagen) # Se muestra la imagen 1
 
-h = np.zeros((480, 640), dtype=np.uint8) + 135 # Se asigna el nivel al campo de 'hue' para el color magenta
+s = np.zeros((480, 640), dtype=np.uint8) + 150 # Se asigna el nivel al campo 'saturation'
 hsv = cv2.merge((h, s, v)) # Se mezclan los canales 'hue', 'saturation' y 'value'
 imagen = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR) # Se convierte el espacio de color de 'HSV' a 'BGR'
 cv2.imshow("Imagen 4", imagen) # Se muestra la imagen 1
 
-h = np.zeros((480, 640), dtype=np.uint8) + 120 # Se asigna el nivel al campo de 'hue' para el color azul
+s = np.zeros((480, 640), dtype=np.uint8) + 200 # Se asigna el nivel al campo 'saturation'
 hsv = cv2.merge((h, s, v)) # Se mezclan los canales 'hue', 'saturation' y 'value'
 imagen = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR) # Se convierte el espacio de color de 'HSV' a 'BGR'
 cv2.imshow("Imagen 5", imagen) # Se muestra la imagen 1
