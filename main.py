8c848ba50623db8151fc356276c34df08ac7bf3f
ian16munoz3nunez1 <ianemn1603@gmail.com>
2023-01-04 17:54:30 -0600

    Ejercicio de mascaras (GreenScreen)

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 import cv2 # Se importa opencv
 
-imagen = cv2.imread("ejercicio5.png") # Se lee la imagen
+# Se leen las imagenes
+sonic = cv2.imread("sonic.jpg")
+gato = cv2.imread("gato.jpg")
+explo = cv2.imread("explo.jpg")
 
-escala = 0.5
-imagen = cv2.resize(imagen, None, fx=escala, fy=escala) # Se escala el tamaño de la imagen
+sonic = cv2.resize(sonic, None, fx=0.3, fy=0.3) # Se escala la imagen
+gato = cv2.resize(gato, None, fx=0.45, fy=0.45) # Se escala la imagen
+explo = cv2.resize(explo, None, fx=0.2, fy=0.2) # Se escala la imagen
 
-imagenHSV = cv2.cvtColor(imagen, cv2.COLOR_BGR2HSV) # Se convierte la imagen de BGR a HSV
+sonicHSV = cv2.cvtColor(sonic, cv2.COLOR_BGR2HSV) # Se convierte la imagen de BGR a HSV
+gatoHSV = cv2.cvtColor(gato, cv2.COLOR_BGR2HSV) # Se convierte la imagen de BGR a HSV
+exploHSV = cv2.cvtColor(explo, cv2.COLOR_BGR2HSV) # Se convierte la imagen de BGR a HSV
 
-mask = cv2.inRange(imagenHSV, (55, 250, 235), (60, 255, 240)) # Se obtiene la mascara de la imagen
+sonicMask = cv2.inRange(sonicHSV, (55, 240, 240), (65, 255, 255)) # Se obtiene la mascara de la imagen
+gatoMask = cv2.inRange(gatoHSV, (55, 240, 240), (65, 255, 255)) # Se obtiene la mascara de la imagen
+exploMask = cv2.inRange(exploHSV, (55, 160, 160), (65, 245, 255)) # Se obtiene la mascara de la imagen
 
-cv2.imshow("Imagen", imagen) # Se muestra la imagen original
-cv2.imshow("Mascara", mask) # Se muestra la mascara de imagen
+cv2.imshow("Sonic", sonic) # Se muestra la imagen original
+cv2.imshow("Sonic (Mascara)", sonicMask) # Se muestra la mascara de la imagen
+
+cv2.imshow("Gato", gato) # Se muestra la imagen original
+cv2.imshow("Gato (Mascara)", gatoMask) # Se muestra la mascara de la imagen
+
+cv2.imshow("Explosion", explo) # Se muestra la mascara original
+cv2.imshow("Explosion (Mascara)", exploMask) # Se muestra la mascara de la imagen
 
 # Se espera a que el usuario presione la tecla 'esc'
 while True:
