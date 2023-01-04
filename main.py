e4bc43c6085ecbb72038f39a805b7fea85935a43
ian16munoz3nunez1 <ianemn1603@gmail.com>
2023-01-04 17:30:21 -0600

    Ejercicio 5

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import cv2 # Se importa opencv
 
-imagen = cv2.imread("ejercicio4.png") # Se lee la imagen
+imagen = cv2.imread("ejercicio5.png") # Se lee la imagen
 
 escala = 0.5
 imagen = cv2.resize(imagen, None, fx=escala, fy=escala) # Se escala el tamaño de la imagen
 
 imagenHSV = cv2.cvtColor(imagen, cv2.COLOR_BGR2HSV) # Se convierte la imagen de BGR a HSV
 
-mask = cv2.inRange(imagenHSV, (155, 140, 160), (165, 150, 170)) # Se obtiene la mascara de la imagen
+mask = cv2.inRange(imagenHSV, (55, 250, 235), (60, 255, 240)) # Se obtiene la mascara de la imagen
 
 cv2.imshow("Imagen", imagen) # Se muestra la imagen original
 cv2.imshow("Mascara", mask) # Se muestra la mascara de imagen
