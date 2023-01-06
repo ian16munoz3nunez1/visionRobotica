1fa1a2d4b0c69e9e8960aa75dd4024b3c2c4616e
ian16munoz3nunez1 <ianemn1603@gmail.com>
2023-01-06 17:38:42 -0600

    Ejercicio 2

--- main.py
+++ main.py
@@ -9,18 +9,21 @@ morado = (255, 0, 255)
 
 # Se dibujan las figuras en la imagen
 cv2.circle(imagen, (650, 450), 90, blanco, -1)
-cv2.circle(imagen, (160, 220), 50, blanco, -1)
-cv2.circle(imagen, (250, 430), 150, blanco, -1)
-cv2.rectangle(imagen, (50, 50), (100, 100), blanco, -1)
-cv2.rectangle(imagen, (300, 40), (750, 250), blanco, -1)
-cv2.rectangle(imagen, (440, 300), (540, 400), blanco, -1)
+cv2.circle(imagen, (160, 150), 50, blanco, -1)
+cv2.circle(imagen, (200, 430), 150, blanco, -1)
+cv2.circle(imagen, (400, 100), 40, blanco, -1)
+cv2.circle(imagen, (700, 200), 60, blanco, -1)
+cv2.circle(imagen, (580, 80), 20, blanco, -1)
+cv2.circle(imagen, (470, 250), 80, blanco, -1)
 
 grises = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY) # Se convierte la imagen a escala de grises
 
 # Se obtienen los contornos de las figuras con la imagen en escala de grises
 cnts, _ = cv2.findContours(grises, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-cv2.drawContours(imagen, cnts, -1, morado, 2) # Se dibujan los contornos en la imagen original
+for c in cnts:
+    x, y, w, h = cv2.boundingRect(c) # Se toman las posiciones y medidas de cada contorno
+    cv2.rectangle(imagen, (x, y), (x+w, y+h), morado, 2) # Se dibuja un rectangulo que encierra la figura
 
 cv2.imshow("Imagen", imagen) # Se muestra la imagen
 
