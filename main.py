f753e8f87d35bf812283f41c0f0a8496dad0686a
ian16munoz3nunez1 <ianemn1603@gmail.com>
2023-01-07 02:52:30 -0600

    Punto A

---
+++ main.py
@@ -0,0 +1,22 @@
+import cv2 # Se importa 'opencv'
+import numpy as np # Se importa 'numpy' como np
+
+imagen = cv2.imread("rubik.jpg") # Se lee la imagen
+
+escala = 0.3
+imagen = cv2.resize(imagen, None, fx=escala, fy=escala) # Se cambian las dimensiones de la imagen
+
+kernel = np.ones((3, 3), dtype=np.float32) * 1/9 # Se obtiene el kernel para el filtro de la imagen
+
+filtro = cv2.filter2D(imagen, -1, kernel) # Se aplica el filtro a la imagen
+
+# Se muestran las imagenes
+cv2.imshow("Imagen", imagen)
+cv2.imshow("Filtro", filtro)
+
+# Se espera a que el usuario presione la tecla 'esc'
+while True:
+    if cv2.waitKey(1) == 27:
+        break
+cv2.destroyAllWindows() # Se cierran todas las ventanas
+
