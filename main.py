72a78581fbb2fb7b60c48a07c3d0168e637b12d4
ian16munoz3nunez1 <ianemn1603@gmail.com>
2023-01-08 02:34:12 -0600

    Ejercicio 2

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-# Aplicar una operacion de erosion a la imagen 1
+# Aplicar una operacion de erosion a la imagen 2
 
 import cv2 # Se importa 'opencv'
 import numpy as np # Se importa 'numpy' como np
 
-imagen = cv2.imread("imagen_1.png") # Se lee la imagen
+imagen = cv2.imread("imagen_2.png") # Se lee la imagen
 
 kernel = np.ones((5, 5), np.uint8) # Se crea el kernel para el filtro
 erosion = cv2.erode(imagen, kernel) # Se aplica el filtro de erosion a la imagen
