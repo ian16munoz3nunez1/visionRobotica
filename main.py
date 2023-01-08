b29be5a652e628168f6cc20aadbba217d2ef17a5
ian16munoz3nunez1 <ianemn1603@gmail.com>
2023-01-08 02:48:39 -0600

    Ejercicio 5

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-# Aplicar una operacion de dilatacion a la imagen 2
+# Aplicar una operacion de opening a la imagen 3
 
 import cv2 # Se importa 'opencv'
 import numpy as np # Se importa 'numpy' como np
 
-imagen = cv2.imread("imagen_2.png") # Se lee la imagen
+imagen = cv2.imread("imagen_3.png") # Se lee la imagen
 
-kernel = np.ones((5, 5), np.uint8) # Se crea el kernel para el filtro
-dilatacion = cv2.dilate(imagen, kernel) # Se aplica el filtro de dilatacion a la imagen
+kernel = np.ones((3, 3), np.uint8) # Se crea el kernel para el filtro
+opening = cv2.morphologyEx(imagen, cv2.MORPH_OPEN, kernel) # Se aplica el filtro de opening a la imagen
 
 cv2.imshow("Imagen", imagen) # Se muestra la imagen original
-cv2.imshow("Dilatacion", dilatacion) # Se muestra la imagen con el filtro de dilatacion
+cv2.imshow("Opening", opening) # Se muestra la imagen con el filtro de opening
 
 # Se espera a que el usuario presione la tecla 'esc'
 while True:
