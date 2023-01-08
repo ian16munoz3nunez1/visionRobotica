93b62503a5193c8d3fe29ce7e46b460d411cc91c
ian16munoz3nunez1 <ianemn1603@gmail.com>
2023-01-08 02:38:59 -0600

    Ejercicio 3

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-# Aplicar una operacion de erosion a la imagen 2
+# Aplicar una operacion de dilatacion a la imagen 1
 
 import cv2 # Se importa 'opencv'
 import numpy as np # Se importa 'numpy' como np
 
-imagen = cv2.imread("imagen_2.png") # Se lee la imagen
+imagen = cv2.imread("imagen_1.png") # Se lee la imagen
 
 kernel = np.ones((5, 5), np.uint8) # Se crea el kernel para el filtro
-erosion = cv2.erode(imagen, kernel) # Se aplica el filtro de erosion a la imagen
+dilatacion = cv2.dilate(imagen, kernel) # Se aplica el filtro de dilatacion a la imagen
 
 cv2.imshow("Imagen", imagen) # Se muestra la imagen original
-cv2.imshow("Erosion", erosion) # Se muestra la imagen con el filtro de erosion
+cv2.imshow("Dilatacion", dilatacion) # Se muestra la imagen con el filtro de dilatacion
 
 # Se espera a que el usuario presione la tecla 'esc'
 while True:
