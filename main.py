fba9e1e31b239825889a354593e80729b5486f90
ian16munoz3nunez1 <ianemn1603@gmail.com>
2023-01-08 02:50:10 -0600

    Ejercicio 6

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-# Aplicar una operacion de opening a la imagen 3
+# Aplicar una operacion de opening a la imagen 4
 
 import cv2 # Se importa 'opencv'
 import numpy as np # Se importa 'numpy' como np
 
-imagen = cv2.imread("imagen_3.png") # Se lee la imagen
+imagen = cv2.imread("imagen_4.png") # Se lee la imagen
 
 kernel = np.ones((3, 3), np.uint8) # Se crea el kernel para el filtro
 opening = cv2.morphologyEx(imagen, cv2.MORPH_OPEN, kernel) # Se aplica el filtro de opening a la imagen
