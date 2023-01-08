510ab4a29a08ce858ad92a5d2376562ac86c4fc6
ian16munoz3nunez1 <ianemn1603@gmail.com>
2023-01-08 02:32:55 -0600

    Ejercicio 1

---
+++ main.py
@@ -0,0 +1,19 @@
+# Aplicar una operacion de erosion a la imagen 1
+
+import cv2 # Se importa 'opencv'
+import numpy as np # Se importa 'numpy' como np
+
+imagen = cv2.imread("imagen_1.png") # Se lee la imagen
+
+kernel = np.ones((5, 5), np.uint8) # Se crea el kernel para el filtro
+erosion = cv2.erode(imagen, kernel) # Se aplica el filtro de erosion a la imagen
+
+cv2.imshow("Imagen", imagen) # Se muestra la imagen original
+cv2.imshow("Erosion", erosion) # Se muestra la imagen con el filtro de erosion
+
+# Se espera a que el usuario presione la tecla 'esc'
+while True:
+    if cv2.waitKey(1) == 27:
+        break
+cv2.destroyAllWindows() # Se cierran todas las ventanas
+
