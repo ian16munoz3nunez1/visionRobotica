62511f6a9438994a1e047d53c50f948ee8843c2e
ian16munoz3nunez1 <ianemn1603@gmail.com>
2023-01-08 03:03:30 -0600

    Ejercicio 8

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-# Aplicar una operacion de closing a la imagen 5
+# Aplicar una operacion de closing a la imagen 6
 
 import cv2 # Se importa 'opencv'
 import numpy as np # Se importa 'numpy' como np
 
-imagen = cv2.imread("imagen_5.png") # Se lee la imagen
+imagen = cv2.imread("imagen_6.png") # Se lee la imagen
 
 kernel = np.ones((3, 3), np.uint8) # Se crea el kernel para el filtro
 closing = cv2.morphologyEx(imagen, cv2.MORPH_CLOSE, kernel) # Se aplica el filtro de closing a la imagen
