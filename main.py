c8627873ce9b63fa1202a1c8b86e6f0e644165af
Ian Muñoz Nuñez <ianemn@ian.kali>
2023-06-24 20:26:08 -0600

    Centroide
    Se calcula y dibuja el centroide de un color específico
    encontrado en la imagen.

--- main.py
+++ main.py
@@ -1,4 +1,39 @@
-import cv2 # Se importa la opencv --> pip install opencv-contrib-python
-import numpy as np # Se importa la libreria numpy como np --> pip install numpy
-import matplotlib.pyplot as plt # Se importa la libreria pyplot --> pip install matplotlib
+import cv2 as cv
+import numpy as np
+
+captura = cv.VideoCapture(0) # Conexion con la camara 0/2
+
+kernel = np.ones((9, 9), dtype=np.uint8) # Kernel usado para la erosion
+
+while True:
+    leido, video = captura.read() # Lectura de la camara
+
+    if not leido: # Si hay un error con la lectura se termina el ciclo
+        break
+
+    if cv.waitKey(1) == 27: # Si se presiona 'esc' se termina el ciclo
+        break
+
+    video = cv.resize(video, None, fx=0.4, fy=0.4) # Redimension del video
+    m, n = video.shape[0:2] # Dimensiones del video
+
+    hsv = cv.cvtColor(video, cv.COLOR_BGR2HSV) # Espacio de color de BGR a HSV
+    mask = cv.inRange(hsv, (40, 20, 20), (80, 250, 250)) # Mascara de color
+    mask = cv.normalize(mask.astype(float), None, 0.0, 1.0, cv.NORM_MINMAX) # Normalizacion de 0 a 1
+    mask = cv.erode(mask, kernel) # Aplicacion de la erosion
+
+    posy, posx = np.where(mask==1) # Posiciones de los 1's
+    try:
+        cy = int(np.sum(posy)/np.sum(mask)) # Posicion del centroide en 'y'
+        cx = int(np.sum(posx)/np.sum(mask)) # Posicion del centroide en 'x'
+    except:
+        cy = int(m/2) # Posicion del centroide en 'y'
+        cx = int(n/2) # Posicion del centroide en 'x'
+    cv.circle(video, (cx, cy), 5, (0, 255, 0), -1) # Circulo del centroide en el video
+
+    cv.imshow("Video", video) # Se muestra el video
+    cv.imshow("Mask", mask) # Se muestra la mascara
+
+captura.release() # Se libera la captura
+cv.destroyAllWindows() # Se cierran todas las ventanas
 
