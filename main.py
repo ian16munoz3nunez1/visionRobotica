9d346fd1ae231b9cc001055d863d41cb4b976962
Ian Muñoz Nuñez <ianemn@ian.kali>
2023-06-24 23:22:41 -0600

    Registro de videos y valores
    Se escriben los videos de entrada y salida en archivos .mp4,
    también se guardan los valores de ángulos, los errores y la
    posición del centroide en arreglos y al terminar el
    seguimiento, se muestran gráficas con estos valores.

--- main.py
+++ main.py
@@ -2,10 +2,25 @@
 
 import cv2 as cv
 import numpy as np
+import matplotlib.pyplot as plt
 import serial
+import timeit
 from time import sleep
 
+angle_plot = np.array([[], []]) # Arreglo para guardar los angulos
+error_plot = np.array([[], []]) # Arreglo para guardar el error
+c_plot = np.array([[], []]) # Arreglo para guardar la posicion del centroide
+t_plot = np.array([]) # Arreglo para guardar el tiempo transcurrido
+
 captura = cv.VideoCapture(2) # Conexion con la camara 0/2
+fps = int(captura.get(cv.CAP_PROP_FPS)) # Frames por segundo de la camara usada
+fourcc = cv.VideoWriter_fourcc(*'mp4v') # Codec para guardar los videos
+
+# Variables usadas para escribir los datos de los videos
+outVideo = cv.VideoWriter('video.mp4', fourcc, fps, (180, 180))
+outMask = cv.VideoWriter('mask.mp4', fourcc, fps, (180, 180))
+outNoise = cv.VideoWriter('noise.mp4', fourcc, fps, (180, 180))
+outFilter = cv.VideoWriter('filter.mp4', fourcc, fps, (180, 180))
 
 kernel = np.ones((9, 9), dtype=np.uint8) # Kernel usado para la erosion
 f = np.ones((11, 11), dtype=np.float32)*1/121 # Kernel para aplicar un filtro de desenfoque al video con ruido
@@ -17,6 +32,10 @@ angleY = chr(90) # Inicializacion del angulo 'y' en 90 grados
 angleX = chr(90) # Inicializacion del angulo 'x' en 90 grados
 minAngle = 45 # Angulo minimo al que deben girar los motores
 maxAngle = 135 # Angulo maximo al que deben girar los motores
+
+# Se toma el tiempo actual
+start = timeit.default_timer()
+end = start
 
 while True:
     leido, video = captura.read() # Lectura de la camara
@@ -38,13 +57,13 @@ while True:
 
     hsv = cv.cvtColor(videoFilter, cv.COLOR_BGR2HSV) # Espacio de color de BGR a HSV
     mask = cv.inRange(hsv, (40, 20, 20), (80, 250, 250)) # Mascara de color
-    mask = cv.normalize(mask.astype(float), None, 0.0, 1.0, cv.NORM_MINMAX) # Normalizacion de 0 a 1
     mask = cv.erode(mask, kernel) # Aplicacion de la erosion
+    norm = cv.normalize(mask.astype(float), None, 0.0, 1.0, cv.NORM_MINMAX) # Normalizacion de 0 a 1
 
-    posy, posx = np.where(mask==1) # Posiciones de los 1's
+    posy, posx = np.where(norm==1) # Posiciones de los 1's
     try:
-        cy = int(np.sum(posy)/np.sum(mask)) # Posicion del centroide en 'y'
-        cx = int(np.sum(posx)/np.sum(mask)) # Posicion del centroide en 'x'
+        cy = int(np.sum(posy)/np.sum(norm)) # Posicion del centroide en 'y'
+        cx = int(np.sum(posx)/np.sum(norm)) # Posicion del centroide en 'x'
     except:
         cy = int(m/2) # Posicion del centroide en 'y'
         cx = int(n/2) # Posicion del centroide en 'x'
@@ -75,7 +94,57 @@ while True:
     cv.imshow("VideoNoise", videoNoise) # Se muestra el video con ruido
     cv.imshow("VideoFilter", videoFilter) # Se muestra el video con filtro aplicado
 
+    outVideo.write(video) # Se escribe el video original
+    outMask.write(cv.cvtColor(mask, cv.COLOR_GRAY2BGR)) # Se escribe la mascara del video
+    outNoise.write(videoNoise) # Se escribe el video con ruido
+    outFilter.write(videoFilter) # Se escribe el video filtrado
+
+    end = timeit.default_timer() # Se toma el tiempo actual
+    t = end - start # Diferencia de tiempo
+    t_plot = np.hstack((t_plot, t)) # Se guarda el tiempo transcurrido
+    angle_plot = np.hstack((angle_plot, np.array([[ord(angleX)], [ord(angleY)]]))) # Se guardan los angulos de los servos
+    error_plot = np.hstack((error_plot, np.array([[e_x], [e_y]]))) # Se guarda el error
+    c_plot = np.hstack((c_plot, np.array([[cx], [cy]]))) # Se guarda la posicion del centroide
+
 uno.close() # Cierre de la conexion serial con la Arduino Uno
 captura.release() # Se libera la captura
 cv.destroyAllWindows() # Se cierran todas las ventanas
+
+# Grafica de los angulos de los motores
+plt.figure(1)
+plt.grid()
+
+plt.plot(t_plot, angle_plot[0, :], linewidth=2)
+plt.plot(t_plot, angle_plot[1, :], linewidth=2)
+
+plt.title("Angulos x/y", fontsize=20)
+plt.xlabel('Tiempo (s)', fontsize=15)
+plt.ylabel('Angulos', fontsize=15)
+plt.legend(['$angle_x$', '$angle_y$'])
+
+# Grafica del error
+plt.figure(2)
+plt.grid()
+
+plt.plot(t_plot, error_plot[0, :], linewidth=2)
+plt.plot(t_plot, error_plot[1, :], linewidth=2)
+
+plt.title("Errores x/y", fontsize=20)
+plt.xlabel('Tiempo (s)', fontsize=15)
+plt.ylabel('Error', fontsize=15)
+plt.legend(['$e_x$', '$e_y$'])
+
+# Grafica del centroide
+plt.figure(3)
+plt.grid()
+
+plt.plot(t_plot, c_plot[0, :], linewidth=2)
+plt.plot(t_plot, c_plot[1, :], linewidth=2)
+
+plt.title("Centroides x/y", fontsize=20)
+plt.xlabel('Tiempo (s)', fontsize=15)
+plt.ylabel('Centroides', fontsize=15)
+plt.legend(['$c_x$', '$c_y$'])
+
+plt.show()
 
